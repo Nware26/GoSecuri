3f19bad74cb9f45b72fa34c82a8c7a17e027b62c
Oxyrub <tremouille.sylvain@gmail.com>
2019-01-08 13:14:00 +0100

    Sylvain

--- app/src/main/java/teamfrite/com/gosecuri/MainActivity.kt
+++ app/src/main/java/teamfrite/com/gosecuri/MainActivity.kt
@@ -2,11 +2,14 @@ package teamfrite.com.gosecuri
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import teamfrite.com.gosecuri.dao.Dao
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Dao().test()
+        Dao().add()
     }
 }
